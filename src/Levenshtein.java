c08be46f5da76cef5e75a27b16f5dbf2079e90b6
Stephen Walker <stephenleewalker@gmail.com>
2015-05-05 18:57:20 -0500

    Refactoring

--- src/Levenshtein.java
+++ src/Levenshtein.java
@@ -1,17 +1,34 @@
 
 public class Levenshtein {
 	
-	private static void setupMatrix(MatrixData m)
+	private final int length1;
+	private final int length2;
+	private final String string1;
+	private final String string2;
+	private final int[][] lValues;
+	
+	public Levenshtein(String s1, String s2) {
+		string1 = s1.toLowerCase();
+		string2 = s2.toLowerCase();
+		length1 = string1.length();
+		length2 = string2.length();
+		lValues = new int[length1+1][length2+1];
+		setupMatrix();
+		calculateMatrix();
+		
+	}
+	
+	private void setupMatrix()
 	{
-		for(int i=0; i<=m.length1(); i++) {
-			m.setValue(i,0,i);
+		for(int i=0; i<=length1; i++) {
+			lValues[i][0] = i;
 		}
-		for(int j=0; j<=m.length2(); j++) {
-			m.setValue(0,j,j);
+		for(int j=0; j<=length2; j++) {
+			lValues[0][j] = j;
 		}
 	}
 	
-	private static int smallestValue(int v1, int v2, int v3)
+	private int smallestValue(int v1, int v2, int v3)
 	{
 		int smallest = v1;
 		if(v2 < smallest)
@@ -21,56 +38,33 @@ public class Levenshtein {
 		return smallest;
 	}
 	
-	private static void calculateMatrix(MatrixData m)
+	private void calculateMatrix()
 	{
 		int insertionValue;
 		int deletionValue;
 		int substitutionValue;
 		
-		for(int i=1; i<= m.length1(); i++) {
-			for(int j=1; j<=m.length2(); j++) {
-				insertionValue = m.matrix()[i][j-1] + 1;
-				deletionValue = m.matrix()[i-1][j] + 1;
-				substitutionValue = m.matrix()[i-1][j-1];
-				if(m.string1().charAt(i-1) != m.string2().charAt(j-1)) {
+		for(int i=1; i<= length1; i++) {
+			for(int j=1; j<=length2; j++) {
+				insertionValue = lValues[i][j-1] + 1;
+				deletionValue = lValues[i-1][j] + 1;
+				substitutionValue = lValues[i-1][j-1];
+				if(string1.charAt(i-1) != string2.charAt(j-1)) {
 					substitutionValue += 2;
 				}
-				m.setValue(i,j,smallestValue(insertionValue, deletionValue, substitutionValue));
+				lValues[i][j] = smallestValue(insertionValue, deletionValue, substitutionValue);
 			}
 		}
 	}
 	
-	public static int[][] values(String s1, String s2)
+	public int[][] values()
 	{
-		int length1 = s1.length();
-		int length2 = s2.length();
-		s1 = s1.toLowerCase();
-		s2 = s2.toLowerCase();
-		int[][] lValues = new int[length1+1][length2+1];
-		MatrixData m = new MatrixData(lValues, s1, length1, s2, length2);
-
-		setupMatrix(m);
-		calculateMatrix(m);
-		return m.matrix();
+		return lValues;
 	}
 	
-	public static int editDistance(String s1, String s2)
+	public int editDistance()
 	{
-		int length1 = s1.length();
-		int length2 = s2.length();
-		s1 = s1.toLowerCase();
-		s2 = s2.toLowerCase();
-		if (s1.equals(s2))
-			return 0;
-		else
-		{		
-			if (length1 > length2)
-				return length1-length2;
-			else if (length1 < length2)
-				return length2-length1;
-			else
-				return 2;
-		}
+		return lValues[length1][length2];
 	}
 }
 
--- src/LevenshteinTest.java
+++ src/LevenshteinTest.java
@@ -1,29 +1,37 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class LevenshteinTest {
+	
+	private String s1;
+	private String s2;
+	private Levenshtein l1;
+	private Levenshtein l2;
+	
+	@Before
+	public void setup() {
+		s1 = "a";
+		s2 = "b";
+		l1 = new Levenshtein(s1, s1);
+		l2 = new Levenshtein(s1, s2);
+	}
 
 	@Test
 	public void getASimpleEditDistance() {
-		String s1 = "Test1";
-		String s2 = "Test2";
-		assertNotNull(Levenshtein.editDistance(s1, s2));
+		assertNotNull(l1.editDistance());
 	}
 	
 	@Test
 	public void twoCharactersAreEqaul() {
-		String s1 = "a";
-		String s2 = "a";
-		assertEquals(0, Levenshtein.editDistance(s1, s2));
+		assertEquals(0, l1.editDistance());
 	}
 	
 	@Test
 	public void twoCharactersAreNotEqual() {
-		String s1 = "a";
-		String s2 = "b";
-		assertNotEquals(0, Levenshtein.editDistance(s1, s2));
+		assertNotEquals(0, l2.editDistance());
 	}
 	
 	@Test
@@ -31,8 +39,10 @@ public class LevenshteinTest {
 		String s1 = "";
 		String s2 = "a";
 		String s3 = "ab";
-		assertEquals(1, Levenshtein.editDistance(s2, s1));
-		assertEquals(2, Levenshtein.editDistance(s3, s1));
+		Levenshtein l1 = new Levenshtein(s2, s1);
+		Levenshtein l2 = new Levenshtein(s3, s1);
+		assertEquals(1, l1.editDistance());
+		assertEquals(2, l2.editDistance());
 	}
 	
 	@Test
@@ -40,30 +50,29 @@ public class LevenshteinTest {
 		String s1 = "";
 		String s2 = "a";
 		String s3 = "ab";
-		assertEquals(1, Levenshtein.editDistance(s1, s2));
-		assertEquals(2, Levenshtein.editDistance(s1, s3));
+		Levenshtein l1 = new Levenshtein(s1, s2);
+		Levenshtein l2 = new Levenshtein(s1, s3);
+		assertEquals(1, l1.editDistance());
+		assertEquals(2, l2.editDistance());
 	}
 	
 	@Test
 	public void canDetectSubstitution() {
-		String s1 = "a";
-		String s2 = "b";
-		assertEquals(2, Levenshtein.editDistance(s1, s2));
+		assertEquals(2, l2.editDistance());
 	}
 	
 	@Test
 	public void canGetSimpleLevenshteinMatrix(){
-		String s1 = "a";
-		String s2 = "a";
 		int[][] lv = new int[][]{{0, 1},
 								 {1, 0}};
-		assertArrayEquals(lv, Levenshtein.values(s1, s2));
+		assertArrayEquals(lv, l1.values());
 	}
 	
 	@Test
 	public void canGetComplexLevenshteinMatrix() {
 		String s1 = "Intention";
 		String s2 = "Execution";
+		Levenshtein l1 = new Levenshtein(s1, s2);
 		int[][] lv = new int[][]{{0,1,2,3,4,5,6,7,8,9},
 								 {1,2,3,4,5,6,7,6,7,8},
 								 {2,3,4,5,6,7,8,7,8,7},
@@ -75,7 +84,9 @@ public class LevenshteinTest {
 								 {8,7,8,9,10,11,10,9,8,9},
 								 {9,8,9,10,11,12,11,10,9,8}};
 			
-		assertArrayEquals(lv, Levenshtein.values(s1, s2));
+		assertArrayEquals(lv, l1.values());
 	}
+	
+	//TODO: Backtrace
 
 }
