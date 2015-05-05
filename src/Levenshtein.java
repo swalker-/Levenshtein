ba403659898f201f8800ef059000ed84a233aaef
Stephen Walker <stephenleewalker@gmail.com>
2015-05-04 21:26:07 -0500

    Refactoring

--- src/Levenshtein.java
+++ src/Levenshtein.java
@@ -1,13 +1,42 @@
 
 public class Levenshtein {
 	
-	private static void setupArray(int[][] lValues, int length1, int length2)
+	private static void setupMatrix(MatrixData m)
 	{
-		for(int i=0; i<=length1; i++) {
-			lValues[i][0] = i;
+		for(int i=0; i<=m.length1(); i++) {
+			m.setValue(i,0,i);
 		}
-		for(int j=0; j<=length2; j++) {
-			lValues[0][j] = j;
+		for(int j=0; j<=m.length2(); j++) {
+			m.setValue(0,j,j);
+		}
+	}
+	
+	private static int smallestValue(int v1, int v2, int v3)
+	{
+		int smallest = v1;
+		if(v2 < smallest)
+			smallest = v2;
+		if(v3 < smallest)
+			smallest = v3;
+		return smallest;
+	}
+	
+	private static void calculateMatrix(MatrixData m)
+	{
+		int insertionValue;
+		int deletionValue;
+		int substitutionValue;
+		
+		for(int i=1; i<= m.length1(); i++) {
+			for(int j=1; j<=m.length2(); j++) {
+				insertionValue = m.matrix()[i][j-1] + 1;
+				deletionValue = m.matrix()[i-1][j] + 1;
+				substitutionValue = m.matrix()[i-1][j-1];
+				if(m.string1().charAt(i-1) != m.string2().charAt(j-1)) {
+					substitutionValue += 2;
+				}
+				m.setValue(i,j,smallestValue(insertionValue, deletionValue, substitutionValue));
+			}
 		}
 	}
 	
@@ -18,32 +47,11 @@ public class Levenshtein {
 		s1 = s1.toLowerCase();
 		s2 = s2.toLowerCase();
 		int[][] lValues = new int[length1+1][length2+1];
-		int insertionValue;
-		int deletionValue;
-		int substitutionValue;
-		int smallestValue;
-		setupArray(lValues, length1, length2);
-		for(int i=1; i<= length1; i++) {
-			for(int j=1; j<=length2; j++) {
-				insertionValue = lValues[i][j-1] + 1;
-				smallestValue = insertionValue;
-				deletionValue = lValues[i-1][j] + 1;
-				substitutionValue = lValues[i-1][j-1];
-				if(s1.charAt(i-1) != s2.charAt(j-1)) {
-					substitutionValue += 2;
-				}
-				if(deletionValue < smallestValue)
-					smallestValue = deletionValue;
-				if(substitutionValue < smallestValue)
-					smallestValue = substitutionValue;
-				if(j==1 && i==4)
-				{
-					System.out.println(s1.charAt(i-1) + "; " + s2.charAt(j-1));
-				}
-				lValues[i][j] = smallestValue;
-			}
-		}
-		return lValues;
+		MatrixData m = new MatrixData(lValues, s1, length1, s2, length2);
+
+		setupMatrix(m);
+		calculateMatrix(m);
+		return m.matrix();
 	}
 	
 	public static int editDistance(String s1, String s2)
@@ -65,3 +73,5 @@ public class Levenshtein {
 		}
 	}
 }
+
+
